11c44931a5fce8a48a6f5775f119705bbbbac163
william hancharek <william.hancharek@gmail.com>
2017-08-16 14:23:09 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit)
+    self.balance += deposit
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+  def valid?
+    self.status == "open" && self.balance > 0 ? true : false
+  end
+
+  def Transfer(sender, receiver, amount)
+    transfer = Transfer.new(sender, receiver, amount)
+  end
+
 end
