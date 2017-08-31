7b7e3dc2c23d52dd6a4105872feb889a26b2d3a2
Lashaunna Samuels <shaunna191@hotmail.com>
2017-08-31 18:15:25 -0400

    Figuring out how to execute a transaction only once

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
-  # your code here
+  attr_accessor :amount, :sender, :status
+  attr_reader :receiver
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+    @amount = 50
+  end
+
+  def valid?
+    receiver.valid? && sender.valid? ? true : false
+  end
+
+  def execute_transaction
+    # binding.pry
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status ? @status = 'complete' : @status = 'pending'
+    
+  end
 end
