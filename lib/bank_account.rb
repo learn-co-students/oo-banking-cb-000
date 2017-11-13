24d8561bde09c13ef247d97a1336dbd803ed12fe
Gerald Ingraham Ingraham <gingraham@icloud.com>
2017-11-13 02:20:40 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status  = 'open'
+
+  end
+
+  def deposit(amount)
+    self.balance += amount.to_i
+  end
+
+  def display_balance
+     "Your balance is $#{balance}."
+  end
+
+  def valid?
+    @balance > 0 && @status == "open" ? true : false
+  end
+
+  def close_account
+        @status = "closed"
+  end
 end
