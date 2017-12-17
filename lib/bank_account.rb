9b572083ae644724360e3a43756935073a0e1cc8
Jen Poole <contact.vandepool@gmail.com>
2017-12-17 21:55:49 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,7 @@ class BankAccount
   # can't change its name
   attr_reader :name
   attr_accessor :balance, :status
-  
+
    # can initialize a Bank Account
    # initializes with a name
    # always initializes with balance of 1000
@@ -11,5 +11,22 @@ class BankAccount
      @name = name
      @balance = 1000
      @status = 'open'
+   end
+
+   # can deposit money into its account
+   def deposit(deposit_amount)
+     @balance = @balance + deposit_amount
+   end
+
+   # can display its balance
+   def display_balance
+   end
+
+   # is valid with an open status and a balance greater than 0
+   def valid?
+   end
+
+   # can close its account
+   def close_account
    end
 end
