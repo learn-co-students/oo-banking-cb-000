66f1bd314a9ded7482c22fcebc4cdc6cf2fad30b
Jen Poole <contact.vandepool@gmail.com>
2017-12-17 22:07:25 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,15 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  # can initialize a Transfer
+  # initializes with a sender
+  # initializes with a receiver
+  # always initializes with a status of 'pending'
+  # initializes with a transfer amount
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = 'pending'
+    @amount = amount 
+  end
 end
