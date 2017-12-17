cb472da3cc7ce55e5b1145c86a7e2883eaa83e36
Jen Poole <contact.vandepool@gmail.com>
2017-12-17 21:52:20 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,15 @@
 class BankAccount
-
+  # can't change its name
+  attr_reader :name
+  attr_accessor :balance, :status
+  
+   # can initialize a Bank Account
+   # initializes with a name
+   # always initializes with balance of 1000
+   # always initializes with a status of 'open'
+   def initialize(name)
+     @name = name
+     @balance = 1000
+     @status = 'open'
+   end
 end
